3f14401d22545e823bbc76bed6ffe13efd40f934
Thrashattack <henrique202@gmail.com>
2023-11-11 20:58:59 +0100

    fix: init with default args

--- battleship.rb
+++ battleship.rb
@@ -2,8 +2,10 @@
 
 require('./game')
 
-size = ARGV[0].to_i || 10
-shoots = ARGV[1].to_i || 50
+size = ARGV[0].to_i.positive? ? ARGV[0].to_i : 10
+
+shoots = ARGV[0].to_i.positive? ? ARGV[1].to_i : 50
+
 debug = ARGV[2] == 'true' || false
 
 Game.new(size:, shoots:, debug:).setup.play
