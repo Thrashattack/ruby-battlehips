8153308fa54fc2bcda864819d08499076988a9db
Thrashattack <henrique202@gmail.com>
2023-11-12 21:41:57 +0100

    feat: ship generation formula cleanup responsabilities

--- app/game.rb
+++ app/game.rb
@@ -2,30 +2,24 @@
 
 module App
   # Game Class
-  class Game
+  class Game < App::IO::STDIO
+    attr_reader :board, :size, :shoots
+
     def initialize(size: 10,
                    shoots: 50,
-                   ships: [Core::Ship.new(size: 1), Core::Ship.new(size: 1), Core::Ship.new(size: 2),
-                           Core::Ship.new(size: 2), Core::Ship.new(size: 3),
-                           Core::Ship.new(size: 4), Core::Ship.new(size: 5)],
-                   debug: false,
-                   log: Utils::GameLog.new)
+                   debug: false)
+      super(game: self)
       @size = size
       @shoots = shoots
-      @ships = ships
       @debug = debug
-      @log = log
     end
 
     def setup
-      @board = Core::Board.new(size: @size)
-      @board.fill
+      setup_board
 
-      @ships.each do |ship|
-        cells = @board.place(ship)
+      setup_ships
 
-        @log.logger.debug(cells.map(&:to_s)) if @debug
-      end
+      place_ships
 
       self
     end
@@ -37,10 +31,40 @@ module App
         run_turn
       end
 
-      game_over
+      winner = all_ships_sank?
+      @board.reveal
+      game_over(winner:)
     end
 
     private
+
+    def setup_board
+      @board = Core::Board.new(size: @size)
+      @board.fill
+
+      @board
+    end
+
+    def setup_ships
+      @ships = []
+      ship_amount = (@size / 2).to_i + ((@size / 2) / 2).to_i
+      ship_max_size = (@size / 2).to_i
+
+      ship_amount.times do |i|
+        i -= ship_max_size if i + 1 > ship_max_size
+        @ships.push(Core::Ship.new(size: i + 1))
+      end
+    end
+
+    def place_ships
+      @ships.each do |ship|
+        cells = @board.place(ship)
+
+        log_cells(cells) if @debug
+      end
+
+      @ships
+    end
 
     def no_more_shoots?
       !@shoots.positive?
@@ -59,63 +83,8 @@ module App
         print_missed(cell)
       end
 
-      decrease_shoots
-    end
-
-    def decrease_shoots
       @shoots -= 1
-      @log.print(msg: "Remaining shoots: #{@shoots}\n\n", type: :warn)
-      @log.logger.warn("Remaining shoots: #{@shoots}")
-    end
-
-    def ask_player_input
-      @log.print(msg: 'Guess a Coordinate!', type: nil)
-      @log.print(msg: "Format: x,y\n\n", type: :debug)
-      @log.print(msg: @board)
-
-      player_input = $stdin.gets.chomp.to_s
-
-      validate!(player_input)
-    rescue Utils::InvalidInputError => e
-      @log.logger.error(e.msg(@size))
-      @log.clear
-      @log.print(msg: e.msg(@size), type: :error)
-      ask_player_input
-    end
-
-    def validate!(player_input)
-      player_input = player_input.split(',')
-      x = player_input[0].to_i - 1
-      y = player_input[1].to_i - 1
-
-      raise Utils::InvalidInputError unless (x >= 0 && x < @size) && (y >= 0 && y < @size)
-
-      [x, y]
-    end
-
-    def print_hit(cell)
-      @log.clear
-      @log.logger.info("Hit Ship at #{cell}")
-      @log.print(msg: ['Nice aiming!', 'Well Done!', 'Amazing shot!'].sample, type: :info)
-    end
-
-    def print_missed(cell)
-      @log.clear
-      @log.logger.info("Shoot missed at #{cell}")
-      @log.print(msg: 'You missed!', type: :error)
-    end
-
-    def game_over
-      if all_ships_sank?
-        @log.print(msg: 'Congrats! You win! :D', type: :info)
-        @log.logger.info('Winner')
-      else
-        @log.print(msg: 'You lose! More lucky next time! :(', type: :error)
-        @log.logger.fatal('Loser')
-      end
-
-      @board.reveal
-      @log.print(msg: @board)
+      shoots_info
     end
   end
 end
